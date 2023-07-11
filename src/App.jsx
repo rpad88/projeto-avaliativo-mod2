0a9d7c7b1d8f34e4741b0b5e4fd029b586ff3e0a
rpad88 <rodolfo.padilha@gmail.com>
2023-07-11 15:33:38 -0400

    preparando a rota inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import './App.css'
+import LoginPage from './pages/LoginPage'
 
 function App() {
   
   return (
-      <>
-      <p>App is render</p>
-      </>
+      <Router>
+        <Routes>
+          <Route path='/' element={<LoginPage/>} />
+        </Routes>
+      </Router>
   )
 }
 
