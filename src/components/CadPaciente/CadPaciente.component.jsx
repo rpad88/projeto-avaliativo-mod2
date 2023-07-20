3c4f58ad35666c91cd0888437907a7819671705c
Rodolfo <6982920+rpad88@users.noreply.github.com>
2023-07-20 16:34:27 -0400

    adicionado comentarios e o reset() dos inputs

--- src/components/CadPaciente/CadPaciente.component.jsx
+++ src/components/CadPaciente/CadPaciente.component.jsx
@@ -12,6 +12,7 @@ export default function CadPacienteComponent() {
 		register,
 		handleSubmit,
 		setValue,
+		reset,
 		formState: { errors },
 	} = useForm()
 	const cidadeRef = useRef()
@@ -29,7 +30,8 @@ export default function CadPacienteComponent() {
 		if (errors.logradouro || errors.cidade)
 			return alert('você deve inserir um CEP válido')
 		const ok = CadastroService.CadastraPaciente(data)
-		if(ok) setShow(true)
+		if(ok) setShow(true) //Abre a animação confirmando o cadastro.
+		reset() //Limpa os inputs
 	}
 
 	const cepInput = async () => {
@@ -37,6 +39,7 @@ export default function CadPacienteComponent() {
 		try {
 			const data = await CadastroService.GetEndereco(cep)
 
+			//IMPORTANTE setar os valores puxados pela API no input antes de salvar.
 			setValue('rua', data.logradouro)
 			setValue('bairro', data.bairro)
 			setValue('cidade', data.localidade)
@@ -190,6 +193,7 @@ export default function CadPacienteComponent() {
 							<Styled.Input
 								type="tel"
 								name="contatoEmergencia"
+								maxLength={11}
 								{...register('contatoEmergencia', {
 									required: true,
 									minLength: 11,
