bd76f0e90abd3b809600c458b68d17cced23927d
Rodolfo <6982920+rpad88@users.noreply.github.com>
2023-07-20 16:50:33 -0400

    iniciando o sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import './App.css'
 import LoginPage from './pages/Login/LoginPage'
 import CadPacientePage from './pages/CadPaciente/CadPaciente.page'
+import HomePage from './pages/Home/Home.page'
 
 function App() {
   
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path='/' element={<LoginPage/>} />
           <Route path='/cadPaciente' element={<CadPacientePage/>} />
+          <Route path='/home' element={<HomePage/>} />
         </Routes>
       </Router>
   )
