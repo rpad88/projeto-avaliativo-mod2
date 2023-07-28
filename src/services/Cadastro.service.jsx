f7f1288b6de59d5eb8b1cf008d936f97fe70d379
Rodolfo <6982920+rpad88@users.noreply.github.com>
2023-07-28 19:10:12 -0400

    melhorado o retorno do CadastraPaciente

--- src/services/Cadastro.service.jsx
+++ src/services/Cadastro.service.jsx
@@ -1,5 +1,5 @@
 // LOCAL STORAGE
-const localStorageKey = 'lab365:users'
+const localStorageKey = "lab365:users"
 const GetOnLocalStorage = () => {
 	return JSON.parse(localStorage.getItem(localStorageKey)) || []
 }
@@ -23,49 +23,44 @@ const GetEndereco = async (cep) => {
 }
 
 // JSON SERVER
-const PACIENTES_URL = 'http://localhost:3000/pacientes'
-const USERS_URL = 'http://localhost:3000/users'
-
+const PACIENTES_URL = "http://localhost:3000/pacientes"
+const USERS_URL = "http://localhost:3000/users"
 
 // ***** PACIENTES *****
 // PUT
 const CadastraPaciente = async (dadosDoForm) => {
 	try {
-		if(PacienteExists){
-			alert("paciente ja cadastrado")
-			return false
-		}
-
 		const paciente = {
-			method: 'POST',
+			method: "POST",
 			headers: {
-				'Content-Type': 'application/json',
+				"Content-Type": "application/json",
 			},
 			body: JSON.stringify(dadosDoForm),
 		}
 
-		await fetch(PACIENTES_URL, paciente)
+		const ok = await fetch(PACIENTES_URL, paciente)
 			.then((data) => {
 				if (!data.ok) throw Error(data.status)
 				return data.json()
 			})
 			.then((update) => {
-				// console.log(update)
-                return true
+				console.log(`${update.nome} cadastrado`)
+				return true
 			})
+		return ok
 	} catch (error) {
 		console.error(error.message)
 	}
 }
 
 // VERIFICA SE O PACIENTE JÁ EXISTE
-const PacienteExists = async (dadosDoForm) => {
+const PacienteExists = async (cpf) => {
 	try {
 		const arrayDePacientes = await fetch(PACIENTES_URL).then((res) => {
 			return res.json()
 		})
 		// some = alternativa ao find, retorna true assim que encontrar 1 opção correspondente.
-		return arrayDePacientes.some(p => p.cpf === dadosDoForm.cpf)
+		return arrayDePacientes.some((p) => p.cpf === cpf)
 	} catch (error) {
 		console.error(error.message)
 	}
@@ -77,7 +72,7 @@ const VerificaConta = async () => {
 		const users = await fetch(USERS_URL).then((res) => {
 			return res.json()
 		})
-        return users
+		return users
 	} catch (error) {
 		console.error(error.message)
 	}
@@ -89,35 +84,34 @@ const VerificaConta = async () => {
 const CadastraUser = async (dados) => {
 	try {
 		// pega o array no banco
-		const arrayDeUsers = await fetch(USERS_URL).then(res => res.json())
+		const arrayDeUsers = await fetch(USERS_URL).then((res) => res.json())
 		// verifica se o e-mail já existe
-		const userJaExiste = arrayDeUsers.find((u) => u.email === dados.email )
+		const userJaExiste = arrayDeUsers.find((u) => u.email === dados.email)
 
-		if(userJaExiste) return alert("Email já cadastrado.")
+		if (userJaExiste) return alert("Email já cadastrado.")
 
 		const newUser = {
-			method: 'POST',
+			method: "POST",
 			headers: {
-				'Content-Type': 'application/json',
+				"Content-Type": "application/json",
 			},
 			body: JSON.stringify(dados),
 		}
 		await fetch(USERS_URL, newUser).then((data) => {
 			if (!data.ok) throw Error(data.status)
-			return console.info('User cadastrado')
+			return console.info("User cadastrado")
 		})
 	} catch (error) {
 		console.error(error.message)
 	}
 }
 
-const BuscaUser = async (dados) => {
-
-}
+const BuscaUser = async (dados) => {}
 
 export const CadastroService = {
 	GetEndereco,
 	CadastraPaciente,
 	VerificaConta,
 	CadastraUser,
+	PacienteExists,
 }
